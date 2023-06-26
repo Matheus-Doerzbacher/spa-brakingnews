6658e6f6f2db72bdba166d2ebcb2a94293611a73
Matheus Doerzbacher <104528933+MathePlay@users.noreply.github.com>
2023-06-26 19:39:12 -0400

    create data mock

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+export const news = [
+    {
+        title: "Noticia 1",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus accumsan risus eu ultrices. Phasellus laoreet nulla tortor, vel condimentum ipsum dictum sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam at nibh ac lorem dictum sodales et congue nunc. Nam ut eros arcu. Maecenas eros nibh, tincidunt vestibulum congue et, aliquet nec tortor. Nullam mattis mattis dolor at luctus. Donec pellentesque tellus a maximus consectetur. Mauris sit amet iaculis mauris, sed interdum nibh.",
+        banner: "https://static.todamateria.com.br/upload/ge/ne/genero-textual-noticia-og.jpg",
+        likes: 2,
+        comment: 4,
+    },
+    {
+        title: "Noticia 2",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus accumsan risus eu ultrices. Phasellus laoreet nulla tortor, vel condimentum ipsum dictum sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam at nibh ac lorem dictum sodales et congue nunc. Nam ut eros arcu. Maecenas eros nibh, tincidunt vestibulum congue et, aliquet nec tortor. Nullam mattis mattis dolor at luctus. Donec pellentesque tellus a maximus consectetur. Mauris sit amet iaculis mauris, sed interdum nibh.",
+        banner: "https://static.todamateria.com.br/upload/ge/ne/genero-textual-noticia-og.jpg",
+        likes: 2,
+        comment: 4,
+    },
+    {
+        title: "Noticia 3",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus accumsan risus eu ultrices. Phasellus laoreet nulla tortor, vel condimentum ipsum dictum sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam at nibh ac lorem dictum sodales et congue nunc. Nam ut eros arcu. Maecenas eros nibh, tincidunt vestibulum congue et, aliquet nec tortor. Nullam mattis mattis dolor at luctus. Donec pellentesque tellus a maximus consectetur. Mauris sit amet iaculis mauris, sed interdum nibh.",
+        banner: "https://static.todamateria.com.br/upload/ge/ne/genero-textual-noticia-og.jpg",
+        likes: 2,
+        comment: 4,
+    },
+    {
+        title: "Noticia 4",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus accumsan risus eu ultrices. Phasellus laoreet nulla tortor, vel condimentum ipsum dictum sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam at nibh ac lorem dictum sodales et congue nunc. Nam ut eros arcu. Maecenas eros nibh, tincidunt vestibulum congue et, aliquet nec tortor. Nullam mattis mattis dolor at luctus. Donec pellentesque tellus a maximus consectetur. Mauris sit amet iaculis mauris, sed interdum nibh.",
+        banner: "https://static.todamateria.com.br/upload/ge/ne/genero-textual-noticia-og.jpg",
+        likes: 2,
+        comment: 4,
+    }
+]
